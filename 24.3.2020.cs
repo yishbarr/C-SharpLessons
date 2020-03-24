a295aafbe95a70972a9889616ad8f1ad3e80238b
yishbarr <60132436+yishbarr@users.noreply.github.com>
2020-03-24 15:49:34 +0200

    Add files via upload

---
+++ 24.3.2020.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input
+            Console.WriteLine("Write a number for the array length - ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Write a number to multiply the index numbers - ");
+            string b = Console.ReadLine();
+
+            //setup array
+            int i;
+            int lng = Convert.ToInt32(a);
+            int multi = Convert.ToInt32(b);
+            int[] arr = new int[lng];
+            for (i = 0; i < lng; i++)
+                arr[i] = i;
+            for (i = 0; i < lng; i++)
+                arr[i] *= multi;
+            
+            //Output
+            Console.WriteLine($"Number of elements - {lng}");
+            Console.WriteLine($"Multiplication number - {multi}");
+            Console.Write("Array content: ");
+            for (i = 0; i < lng; i++)
+                Console.Write($"{arr[i]}, ");
+
+            Console.ReadKey();
+        }
+    }
+}
