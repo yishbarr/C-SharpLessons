9e929b8e4a7351832433ee4d3e9c97b580a0228e
Yishai Barr <60132436+yishbarr@users.noreply.github.com>
2020-03-31 22:15:14 +0300

    Add files via upload

--- 31.3.2020.cs
+++ 31.3.2020.cs
@@ -7,10 +7,25 @@ namespace Task3
         static void Main(string[] args)
         {
             string[][] array = new string[3][];
-            array[0] = new string[3] {"main", "up", "sub" };
-            array[1] = new string[3] { "down", "center", "up" };
-            array[2] = new string[3] { "sub", "down", "main" };
-
+            array[0]= new string[3];
+            array[1] = new string[3];
+            array[2] = new string[3];
+            for (int rows = 0; rows < array.Length; rows++)
+            {
+                for (int cols = 0; cols < array.Length; cols++)
+                {
+                    if (rows == cols && rows + cols == array.Length - 1)
+                        array[rows][cols] = "centre";
+                    else if (cols == rows)
+                        array[rows][cols] = "main";
+                    else if (cols + rows == array.Length - 1)
+                        array[rows][cols] = "sub";
+                    else if (rows<cols)
+                        array[rows][cols] = "up";
+                    else if (rows>cols)
+                        array[rows][cols] = "down";
+                }
+            }
             foreach (string[] row in array)
             {
                 foreach (string col in row)
