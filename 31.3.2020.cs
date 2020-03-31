a49148adbc9c491ea211ee2c361693049e22c8f9
Yishai Barr <60132436+yishbarr@users.noreply.github.com>
2020-03-31 15:52:20 +0300

    Add files via upload

---
+++ 31.3.2020.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] array = new string[3][];
+            array[0] = new string[3] {"main", "up", "sub" };
+            array[1] = new string[3] { "down", "center", "up" };
+            array[2] = new string[3] { "sub", "down", "main" };
+
+            foreach (string[] row in array)
+            {
+                foreach (string col in row)
+                {
+                    Console.Write(col+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
